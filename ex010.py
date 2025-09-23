e78654a98da5dcae445f5a8f06fa2633efd342b1
Lucas Vieira <l.vieirasdev@gmail.com>
2025-09-23 18:31:52 -0300

    Update ex010.py

--- ex010.py
+++ ex010.py
@@ -1,4 +1,4 @@
 real = float(input('Qual valor voce tem em reais? :'))
 dolar = 3.27
 compra = real/dolar
-print('Com R$ {} voce consegue comprar {} em dolares'.format(real,compra))
+print('Com R$ {} voce consegue comprar {:.2f} em dolares'.format(real,compra))
