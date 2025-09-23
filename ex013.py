742224f8ef4fc4fb24169a390c9659b282fe4587
Lucas Vieira <l.vieirasdev@gmail.com>
2025-09-23 18:17:37 -0300

    exex01300
    Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário  com 15% de aumento

---
+++ ex013.py
@@ -0,0 +1,6 @@
+s1 = float(input('Digite o valor que voce ganha atualmente para saber o seu novo salario: '))
+a = 0.15
+va = s1*a
+vf = va+s1
+print('O seu novo salario vai ser de R${}, foi adicionado R${} ao seu salrio'.format(vf,va))
+
