dd580f5cbf7998e00421e833561ff5c9d8718769
Lucas Vieira <l.vieirasdev@gmail.com>
2025-09-23 17:23:22 -0300

    ex008
    Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros

---
+++ ex008.py
@@ -0,0 +1,5 @@
+n1 = float(input('Digite um numero em metros para que possamos converte-lo para voce :'))
+m = n1
+c = n1*100
+mi =n1*1000
+print('Voce digitou {} metros que e igual a {} centimetros e tambem igual a {} milimetros'.format(m,c,mi))
