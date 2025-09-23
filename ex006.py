3ba06d2f08bcb641417c0a5a172624c662d6edd2
Lucas Vieira <l.vieirasdev@gmail.com>
2025-09-23 18:27:25 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -2,4 +2,4 @@ n1 = int(input('Digite um numero : '))
 x = n1*2
 y = n1*3
 z = n1**(1/2)
-print('O dobro do numero digitado e : {} o triplo e: {} e a raiz quadrada e :{}'.format(x,y,z))
+print('O dobro do numero digitado e : {} o triplo e: {} e a raiz quadrada e :{:.2f}'.format(x,y,z))
