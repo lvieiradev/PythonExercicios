5a02b993ed8d403f559bd2b900a2e661178b6984
Lucas Vieira <l.vieirasdev@gmail.com>
2025-09-23 18:41:13 -0300

    Update ex011.py

--- ex011.py
+++ ex011.py
@@ -1,6 +1,7 @@
 a = float(input('Qual a altura da parede que deja pintar? : '))
 l = float(input('Qual a largura da parede que deseja pintar? :'))
 m2 = a*l
-t = 2
-qt = m2/t
-print('A sua parede tem {} metros quadrados e voce vai precisar de {} litros de tinta para pinta-la!!'.format(m2,qt))
+t = m2/2
+print('A sua parede tem {} metros quadrados e voce vai precisar de {} litros de tinta para pinta-la!!'.format(m2,t))
+
+
