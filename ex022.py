31b84fd5ff3ae115c36bd77f77009838626ff886
Lucas Vieira <l.vieirasdev@gmail.com>
2025-09-29 17:51:21 -0300

    ex022
    Crie um programa que leia o  nome completo de uma pessoa e mostre:
    *O nome com todas as letras maiúsculas
    *O nome com todas minúsculas
    *Quantas letras ao todo(sem considerar espaços)
    Quantas letras tem o primeiro nome

---
+++ ex022.py
@@ -0,0 +1,9 @@
+nome = input('Digite seu nome completo :')
+nome2 = nome.split()
+nomesemespaco = nome.replace(' ','')
+print(nome.upper())
+print(nome.lower())
+print(len(nomesemespaco))
+print(len(nome2[0]))
+
+
