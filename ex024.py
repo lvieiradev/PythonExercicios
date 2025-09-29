ed4bb54701a8eb2bdd876ef455fb3c9ed8e30e4a
Lucas Vieira <l.vieirasdev@gmail.com>
2025-09-29 18:32:06 -0300

    Update ex024.py

--- ex024.py
+++ ex024.py
@@ -1,6 +1,5 @@
-
-
 cidade = input('Digite o nome da sua cidade:')
 print('O nome de sua cidade e: {}'.format(cidade))
-temsanto = 'Sim' if 'santo' in cidade.lower() else 'Nao'
-print('Comeca com Santo? {}'.format(temsanto))
+temsanto = cidade.upper()
+print('A cidade digitada comeca com Santo: {}'.format(temsanto.startswith('SANTO')))
+
