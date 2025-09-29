93ab569f9d0d629f2dde61f3d2ded2e244062ac7
Lucas Vieira <l.vieirasdev@gmail.com>
2025-09-29 18:00:40 -0300

    ex023
    Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.

---
+++ ex023.py
@@ -0,0 +1,6 @@
+numero = input('Digete um numero de 0 a 9999: ')
+numero.split()
+print('Unidade:{}'.format(numero[3]))
+print('Dezena:{}'.format(numero[2]))
+print('Centena:{}'.format(numero[1]))
+print('Milhar:{}'.format(numero[0]))
